08afb24a45ee0de94ef4634351f475b0aee8a202
manik712 <ranineela71@gmail.com>
2024-02-19 22:35:03 +0600

    added the function body

--- javaScriptP05.js
+++ javaScriptP05.js
@@ -1,13 +1,15 @@
- const vowels =['a','e','i','o','u','A','E','I','O','U'];
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
- function foundVowels(sentence){
+function foundVowels(sentence) {
+  let counts = 0;
+  const letters = Array.from(sentence);
 
+  letters.forEach(function (value) {
+    if (vowels.includes(value)) {
+      counts += 1;
+    }
+  });
+  return counts;
+}
 
-
-
-
- }
-
-
-
- console.log();
+console.log(foundVowels("I hate your friend"));
